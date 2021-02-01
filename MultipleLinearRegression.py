73a15a36285fefeea9df5e4688fddc30bcf8e931
AmandaSayuri <32025919+AmandaSayuri@users.noreply.github.com>
2021-02-01 17:28:03 -0300

    Update MultipleLinearRegression.py

--- MultipleLinearRegression.py
+++ MultipleLinearRegression.py
@@ -6,31 +6,38 @@ A base de dados utilizada foi o dataset público auto-mpg.
 Author: Amanda Sayuri Guimarães
 '''
 
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
 import seaborn as sns
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from patsy import dmatrices
 
 
 # Abertura dos dados
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 data = pd.read_csv('data_auto-mpg.csv') 
+
 data = data[data.horsepower != '?']
 data.horsepower = data.horsepower.astype(float)
+data = data.rename(columns={"model year": "year", "car name": "car_name"})
 
-
-# Plot - scatter matrix
+# Plot - não precisa retirar os valores que não são float
+# Seleciona float automaticamente 
 grr = pd.plotting.scatter_matrix(data,figsize=(15, 15), marker='o',
                                  hist_kwds={'bins': 20}, s=60, alpha=.8)
 
 
 # Imprime correlações
 print(data.corr())
+sns.heatmap(data.corr(), annot=True)
+plt.show()
 
 
 # Regressao multipla
 X = data[['cylinders', 'displacement', 'horsepower', 'weight',
-       'acceleration', 'model year', 'origin']]
+       'acceleration', 'year', 'origin']]
 Y = data[['mpg']]
 
 X_ols = sm.add_constant(X) # adding a constant (intercept)
@@ -40,6 +47,7 @@ model.summary()
 
 
 # Alguns plots
+
 influence = model.get_influence()
 student_resid = influence.resid_studentized_external
 leverage = influence.hat_matrix_diag
@@ -53,6 +61,16 @@ sns.regplot(leverage, model.resid_pearson,  fit_reg=False)
 plt.title('Leverage vs. Studentized Residuals')
 plt.xlabel('Leverage')
 plt.ylabel('Studentized Residuals')
+
+
+# Identificando multicolinearidade
+data = data._get_numeric_data() 
+features = "+".join(data.columns)
+y, X = dmatrices('mpg~' + features, data, return_type='dataframe')
+vif = pd.DataFrame()
+vif["VIF Factor"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+vif["features"] = X.columns
+vif.round(1)
 
 
 # Análise final:
